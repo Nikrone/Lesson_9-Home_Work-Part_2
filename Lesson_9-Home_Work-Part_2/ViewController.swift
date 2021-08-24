579ed646fb77c5951465ccabab293da300984516
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-08-24 14:09:16 +0300

    Initial Commit

---
+++ Lesson_9-Home_Work-Part_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_9-Home_Work-Part_2
+//
+//  Created by Evgeniy Nosko on 24.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
