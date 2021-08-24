dbcdbb6af693e643406e854d5fe45ca8f56f506a
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-08-24 16:01:20 +0300

    feat: add implementation Home Work Lesson 9 - Part 2

--- Lesson_9-Home_Work-Part_2/ViewController.swift
+++ Lesson_9-Home_Work-Part_2/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    let movingRedView =  UIView (frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 100)))
+    var initialCenter: CGPoint = .zero
+    let appName = UILabel(frame: CGRect(x: 145, y: 50, width: 150, height: 40))
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        movingRedView.backgroundColor = UIColor.red
+        movingRedView.layer.cornerRadius = 50
+        movingRedView.layer.shadowColor = UIColor.black.cgColor
+        movingRedView.layer.shadowOffset = CGSize(width: 1, height: 1)
+        movingRedView.layer.shadowRadius = 15
+        movingRedView.layer.shadowOpacity = 0.7
+        movingRedView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(movingRedView)
+        movingRedView.center = view.center
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
+        movingRedView.addGestureRecognizer(panGestureRecognizer)
+        
+        appName.text = "Stubborn Ball"
+        view.addSubview(appName)
     }
-
-
+    
+    @objc private func didPan(_ sender:UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            initialCenter = movingRedView.center
+        case .changed:
+            let translation = sender.translation(in: view)
+            
+            movingRedView.center = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+            
+        case .ended,
+                .cancelled:
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: [.curveEaseInOut]) {
+                self.movingRedView.center = self.view.center
+            }
+        default:
+            break
+        }
+    }
+    
 }
-
